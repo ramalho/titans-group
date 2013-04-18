ed8950887ea8d6e6689330a7846952ed9ac55ec5
Luciano Ramalho <luciano@ramalho.org>
2013-04-18 19:09:52 -0300

    novo modulo admin e model Pedido

--- turma1/pizza/entrega/models.py
+++ turma1/pizza/entrega/models.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+
 from django.db import models
 
 class Cliente(models.Model):
@@ -7,11 +8,11 @@ class Cliente(models.Model):
     email = models.EmailField(max_length=64, blank=True)
     logradouro = models.CharField(max_length=128, blank=True)
     numero = models.PositiveIntegerField(default=0)
-    
+
     def __unicode__(self):
         return u'%s (%s)' % (self.nome, self.fone)
-    
-# XXX: mover para o admin.py
 
-from django.contrib import admin
-admin.site.register(Cliente)
+class Pedido(models.Model):
+    cliente = models.ForeignKey(Cliente)
+    data_hora_pedido = models.DateTimeField()
+    data_hora_despacho = models.DateTimeField(null=True, blank=True)
